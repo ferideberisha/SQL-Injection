ec938bcefd1305774bdd2c464a3acc0889dc945b
festinaimeraj <93921647+festinaimeraj@users.noreply.github.com>
2023-01-08 16:19:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -46,3 +46,50 @@ def get_form_details(form):
     method = form.attrs.get("method", "get").lower()
     # marrim të gjitha detajet e input-eve sikur tipi dhe emri
     inputs = []
+    for input_tag in form.find_all("input"):
+        input_type = input_tag.attrs.get("type", "text")
+        input_name = input_tag.attrs.get("name")
+        input_value = input_tag.attrs.get("value", "")
+        inputs.append({"type": input_type, "name": input_name, "value": input_value})
+
+    # vendosim gjithçka në fjalorin e rezultateve
+    details["action"] = action
+    details["method"] = method
+    details["inputs"] = inputs
+    return details
+
+def is_vulnerable(response):
+    #Një funksion i thjeshtë boolean që përcakton nëse një faqe është SQL Injection e cenueshme nga "përgjigja" e saj
+    errors = {
+        # MySQL
+        "you have an error in your sql syntax;",
+        "warning: mysql",
+        # SQL Server
+        "unclosed quotation mark after the character string",
+        # Oracle
+        "quoted string not properly terminated",
+    }
+    for error in errors:
+        # nëse gjen ndonjë prej këtyre error-eve rikthen True
+        if error in response.content.decode().lower():
+            return True
+    # asnjë error nuk është detektuar
+    return False
+
+
+def scan_sql_injection(url):
+    # test në URL
+    for c in "\"'":
+        # shtojmë karakterin e thonjëzës të njëfishtë/dyfishtë në URL
+        new_url = f"{url}{c}"
+        response_field.insert(tk.END, f"\n[!] Trying {new_url}")
+        # bëjmë krkesën HTTP
+        res = s.get(new_url)
+        if is_vulnerable(res):
+            # SQL Injection është detektuar në vet URL-në
+            # nuk ka nevojë të paraprihet për ekstratimin e formave dhe dorëzimin e tyre
+            response_field.insert(tk.END, f"\nSQL Injection vulnerability detected, link: {new_url}")
+            return
+    # test në format HTML
+    forms = get_all_forms(url)
+    response_field.insert(tk.END, f"\n[+] Detected {len(forms)} forms on {url}.")
